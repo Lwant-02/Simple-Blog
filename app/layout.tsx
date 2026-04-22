b17ee23b6b0578a81ac6b4b3b429bfab48679010
Lwant <155357373+Lwant-02@users.noreply.github.com.>
2026-04-22 14:44:06 +0700

    feat:add suppress hydration at root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -66,6 +66,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
+      suppressHydrationWarning
       className={`${outfit.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
